9e536d8589bfcf7e8db188e4538ef778c7b85381
Olori-ebi <ogunleyeseun27@gmail.com>
2021-01-08 09:29:46 +0100

    glitch project

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ import {v4 as uuidv4} from 'uuid';
 
 // Convert csv file to json format
 csvToJson ()
-  .fromFile ('users.csv')
-  .then (users => {
-    fs.writeFile ('users.json', JSON.stringify (users, null, 4), err => {
+  .fromFile ('students.csv')
+  .then (students => {
+    fs.writeFile ('students.json', JSON.stringify (students, null, 4), err => {
       if (err) {
         console.log (err);
       }
-      console.log (users);
+      return students;
     });
   })
   .catch (err => console.log (err));
@@ -20,54 +20,60 @@ csvToJson ()
 
 const app = express ();
 
-const selectedFields = ['Name', 'Age'];
-
-fs.readFile ('./users.json', (err, data) => {
+fs.readFile ('./students.json', (err, data) => {
   if (err) {
     console.log (err);
   }
-  let usersData = JSON.parse (data);
 
-  // Make post request
-  app.post ('/', (req, res) => {
-    const newUser = {
-      Index: '90',
-      Year: '2020',
-      Age: '25',
-      Name: 'Eyiyemi',
-      Movie: 'Ayo ni',
-      field6: '',
-    };
-    // Insert the new data and check if there is/are selected field(s)
-    usersData.push (newUser);
-
-    // if there is/are no selected field(s)
-    if (selectedFields.length === 0) {
-      return res.json ({
-        conversion_key: uuidv4 (),
-        json: usersData,
-      });
+  fs.readFile ('./payload.json', (error, result) => {
+    if (error) {
+      console.log (error);
     } else {
-      function handleObject (OriginalArr, prop) {
-        let newArr = [];
-        let obj = {};
+      const payload = JSON.parse (result);
+      const selectedFields = payload.selectedFields;
+      let studentsData = JSON.parse (data);
 
-        for (let i in OriginalArr) {
-          for (let j in prop) {
-            obj[prop[j]] = OriginalArr[i][prop[j]];
+      // Make post request
+      app.post ('/', (req, res) => {
+        const newstudent = {
+          id: '111114',
+          first_name: 'Eyiyemi',
+          last_name: 'Brown',
+          date_of_birth: '13/1995',
+        };
+
+        // Insert the new data and check if there is/are selected field(s)
+        studentsData.push (newstudent);
+
+        // if there is/are no selected field(s)
+        if (selectedFields.length === 0) {
+          return res.json ({
+            conversion_key: uuidv4 (),
+            json: studentsData,
+          });
+        } else {
+          function handleObject (OriginalArr, prop) {
+            let newArr = [];
+            let obj = {};
+
+            for (let i in OriginalArr) {
+              for (let j in prop) {
+                obj[prop[j]] = OriginalArr[i][prop[j]];
+              }
+              newArr.push ({...obj});
+            }
+            return res.json ({
+              conversion_key: uuidv4 (),
+              json: newArr,
+            });
           }
-          newArr.push ({...obj});
+          handleObject (studentsData, selectedFields);
         }
-        return res.json ({
-          conversion_key: uuidv4 (),
-          json: newArr,
-        });
-      }
-      handleObject (usersData, selectedFields);
+      });
     }
   });
 });
 
-const PORT = 5000;
+const PORT = 6000;
 
 app.listen (PORT, () => console.log (`Server running on port ${PORT}`));
